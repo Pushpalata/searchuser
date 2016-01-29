acd07d84b95d3c5339007ff7548347a5a12bf147
Pushpalata <pushpal.chavhan@gmail.com>
2016-01-29 11:45:54 +0530

    Update dashboard_controller_spec.rb

--- spec/controllers/dashboard_controller_spec.rb
+++ spec/controllers/dashboard_controller_spec.rb
@@ -24,5 +24,57 @@ RSpec.describe DashboardController, type: :controller do
     get :index, { :user_name => "test" }
     assert_response :success
   end
+
+  #should check data fetch from github
+  describe 'Fetch data from github' do
+    it 'returns data from github' do
+      get :index, { :user_name => "Pushpalata"}
+      response = Net::HTTP.get_response(URI("https://api.github.com/users/#{params[:user_name]}"))
+      # Check if it is hit right url
+      assert_match /api.github.com/, response.body
+    end
+    
+    it 'should be json response' do
+      get :index, { :user_name => "Pushpalata"}
+      response = Net::HTTP.get_response(URI("https://api.github.com/users/#{params[:user_name]}"))
+      # the response should be in json format
+      expect(response.content_type).to eq "application/json"
+    end
+  end
+  
+  #should check data fetch from rubygem
+  describe 'Fetch data from rubygem' do
+    it 'returns data from rubygem' do
+      get :index, { :user_name => "Pushpalata"}
+      response = Net::HTTP.get_response(URI("https://rubygems.org/api/v1/profiles/#{params[:user_name]}.json"))
+      # Check if it is hit right url
+      assert_match /rubygems.org/, response.body
+    end
+    
+    it 'should be json response' do
+      get :index, { :user_name => "Pushpalata"}
+      response = Net::HTTP.get_response(URI("https://rubygems.org/api/v1/profiles/#{params[:user_name]}.json"))
+      # the response should be in json format
+      expect(response.content_type).to eq "application/json"
+    end
+  end
+  
+  #should check data fetch from twitter
+  describe 'Fetch data from twitter' do
+    it 'returns data from twitter' do
+      get :index, { :user_name => "Pushpalata"}
+      response = Net::HTTP.get_response(URI("https://api.github.com/users/#{params[:user_name]}"))
+      # Check if it is hit right url
+      assert_match /api.github.com/, response.body
+    end
+    
+    it 'should be json response' do
+      get :index, { :user_name => "Pushpalata"}
+      response = Net::HTTP.get_response(URI("https://api.github.com/users/#{params[:user_name]}"))
+      # the response should be in json format
+      expect(response.content_type).to eq "application/json"
+    end
+  end
   
 end
+
